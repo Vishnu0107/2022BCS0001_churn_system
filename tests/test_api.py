bbcfd158db81b3b3094c3bf4d013f0d3e290309d
Vishnu Narayanan Vinodkumar <vishnunarayananpsv@gmail.com>
2026-03-21 23:56:49 +0530

    Updated the tests and the summary

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,3 +9,43 @@ def test_predict():
         "MonthlyCharges": 80
     })
     assert response.status_code == 200
+
+def test_high_risk_output():
+    response = client.post("/predict", json={
+        "tenure": 5,
+        "MonthlyCharges": 80
+    })
+    assert response.status_code == 200
+    assert response.json()["risk"] == "High Risk"
+
+
+def test_medium_risk_output():
+    response = client.post("/predict", json={
+        "tenure": 10,
+        "MonthlyCharges": 50
+    })
+    assert response.status_code == 200
+    assert response.json()["risk"] == "Medium Risk"
+
+
+def test_low_risk_output():
+    response = client.post("/predict", json={
+        "tenure": 20,
+        "MonthlyCharges": 40
+    })
+    assert response.status_code == 200
+    assert response.json()["risk"] == "Low Risk"
+
+
+def test_missing_tenure():
+    response = client.post("/predict", json={
+        "MonthlyCharges": 50
+    })
+    assert response.status_code == 500
+
+
+def test_missing_monthly_charges():
+    response = client.post("/predict", json={
+        "tenure": 10
+    })
+    assert response.status_code == 500
