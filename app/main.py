f24037b7089b89ab9fdeefa5c6cc2f440b35070a
Vishnu Narayanan Vinodkumar <vishnunarayananpsv@gmail.com>
2026-03-22 19:32:01 +0530

    Added a dataset and a batch prediction

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ from app.database import SessionLocal
 from app.models import Customer
 import logging
 from prometheus_fastapi_instrumentator import Instrumentator 
+import numpy as np
+import time
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
@@ -38,3 +40,65 @@ def predict(data: dict, db=Depends(get_db)):
     logging.info(f"Prediction: {risk}")
 
     return {"risk": risk}
+
+# Generate & Display Dataset
+@app.get("/dataset")
+def get_dataset(n: int = 10):
+    start = time.time()
+    np.random.seed(42)
+    data = [
+        {
+            "id": i,
+            "tenure": int(np.random.randint(1, 72)),
+            "MonthlyCharges": float(np.random.uniform(20, 120))
+        }
+        for i in range(n)
+    ]
+
+    end = time.time()
+
+    return {
+        "total_records": n,
+        "execution_time_seconds": round(end - start, 4),
+        "data": data
+    }
+
+# Batch Prediction 
+@app.get("/batch_predict")
+def batch_predict(n: int = 10):
+    start = time.time()
+
+    np.random.seed(42)
+
+    data = [
+        {
+            "id": i,
+            "tenure": int(np.random.randint(1, 72)),
+            "MonthlyCharges": float(np.random.uniform(20, 120))
+        }
+        for i in range(n)
+    ]
+
+    results = []
+    summary = {"High Risk": 0, "Medium Risk": 0, "Low Risk": 0}
+
+    for d in data:
+        risk = evaluate_risk(d)
+
+        results.append({
+            "id": d["id"],
+            "tenure": d["tenure"],
+            "MonthlyCharges": d["MonthlyCharges"],
+            "risk": risk
+        })
+
+        summary[risk] += 1
+
+    end = time.time()
+
+    return {
+        "total_records": n,
+        "execution_time_seconds": round(end - start, 4),
+        "summary": summary,
+        "results": results
+    }
